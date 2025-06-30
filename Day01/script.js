79ff48c26ba108abc22ea15fec76dcbeecb7731d
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-01 00:11:29 +0530

    First commit with Day01 folder

---
+++ Day01/script.js
@@ -0,0 +1,38 @@
+// interpreter type langugae  in js
+// var a;
+// console.log(a); // value
+// console.log(typeof a); // datatype
+
+// var a = null;
+// console.log(a);  // value
+// console.log(typeof a); // object
+
+// var a = 5;
+// console.log(typeof a); // number
+
+// var a = `a`;
+// console.log(typeof a); // string
+// console.error(typeof a);
+// console.warn(a);
+
+//  -----------------------------------------------------
+
+// console.log(a);
+// var a;    // undefined
+
+//-------------------------------------------------------
+
+// console.log(a); // reference error // temporary dead zone
+// let a= 4;    
+
+// ------------------------------------------------------
+
+// console.log(a);
+// const a =5;
+
+//---------------------------------------------------------
+
+// let a = 5;
+// let a = 6;
+// let a;
+// console.log(a)  // undefined
