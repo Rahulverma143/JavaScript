28f7a692f691a44dff2fe5a3096cf3d23c068447
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-04 16:23:02 +0530

    Day03

---
+++ Day03/script.js
@@ -0,0 +1,12 @@
+// function abc(){
+//     console.log(arguments);
+// }
+// abc(3,4,5,8,5,6,5,4)
+
+
+function greating ( a = 5, b = 5 * 3 ){
+    return a * b;
+}  
+console.log();
+console.log(2);
+console.log(2,5);
