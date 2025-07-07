49dea11e8e9f3697f76aded860dd6445cbd6e5a1
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-07 11:27:35 +0530

    add

--- Arrays/script.js
+++ Arrays/script.js
@@ -100,6 +100,11 @@
 
 
 
+const employees = ["Rahul verma", "verma manotra"];
+let newname = employees.map(
+  (emp) => emp.split(" ")[0] + " " + emp.split(" ")[1].toUpperCase()
+);
+console.log(newname);
 
 
 
