11cef1390db616d5bc818261aaa051bee592186e
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-08 23:45:13 +0530

    files

--- Assignment/Que1.js
+++ Assignment/Que1.js
@@ -1,7 +1,18 @@
-// Add Course Code Prefix
-//  ['Math101', 'Eng102'] → ['COURSE: Math101', 'COURSE: Eng102']
+ /* 1.  Add Course Code Prefix
+ ['Math101', 'Eng102'] → ['COURSE: Math101', 'COURSE: Eng102'] */
 
 
 const courses = ['Math101', 'Eng102'];
 let courseId = courses.map((c) => `COURSE: ${c}`);
 console.log(courseId);
+
+// -----------------------------------------------------------------------
+
+/* 2.  Student Names to Lowercase for Login
+ ['Rahul Sharma', 'Anita Das'] → ['rahul.sharma', 'anita.das'] */
+
+
+
+const  employees= ["Rahul Verma", "Anita Das"];
+let ans = employees.map((e) => e.toLowerCase().replace(" ", "."));
+console.log(ans);
