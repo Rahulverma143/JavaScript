10655f41780f6080103d51d5f4e6c2e19e9d62ef
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-12 23:26:51 +0530

    Add files

--- Assignment/Question.js
+++ Assignment/Question.js
@@ -1,68 +1,68 @@
-// // // const date = new Date();
-// // // console.log(date);
-// // // console.log(typeof date);
-// // // console.log(date.getDay());
-// // // console.log(date.getDate());
-// // // console.log(date.getTime() / (1000 * 60 * 60));
-// // // console.log(date.getHours());
+// const date = new Date();
+// console.log(date);
+// console.log(typeof date);
+// console.log(date.getDay());
+// console.log(date.getDate());
+// console.log(date.getTime() / (1000 * 60 * 60));
+// console.log(date.getHours());
 
 
-// // // const arr1 = [1, 2, 3, 4, 5];
-// // // const arr2 = [2, 3, 4, 5, 6];
-// // // //merge
-// // //  const arr3 = [...arr1, ...arr2];
-// // //  console.log(arr3);
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [2, 3, 4, 5, 6];
+// //merge
+//  const arr3 = [...arr1, ...arr2];
+//  console.log(arr3);
 
-// // // // copy constructor 
-// // // const ans = arr1.push(...arr2);
-// // // console.log(ans); //length of total elements
-// // // console.log(arr1); //merge of ARR1 AND ARR2
+// // copy constructor 
+// const ans = arr1.push(...arr2);
+// console.log(ans); //length of total elements
+// console.log(arr1); //merge of ARR1 AND ARR2
 
-// // // // -----------------------------------------------------
+// -----------------------------------------------------
 
-// // // //pallindrome:
-// // // const str = "maedam";
-// // // const pallindrome = str === str.split("").reverse().join("");
-// // // pallindrome ? console.log("yes") : console.log("no");
+//pallindrome:
+// const str = "maedam";
+// const pallindrome = str === str.split("").reverse().join("");
+// pallindrome ? console.log("yes") : console.log("no");
 
-// // // // ---------------------------------------------------------
+// ---------------------------------------------------------
 
-// // //  const sentence = "Javascript is the wrost language";
-// // //  const senarr = sentence.split(" ");
-// // //  console.log(senarr);
-// // //  const revsen = senarr.map((s) => s.split("").reverse().join(""));
-// // //  console.log(revsen);
+//  const sentence = "Javascript is the wrost language";
+//  const senarr = sentence.split(" ");
+//  console.log(senarr);
+//  const revsen = senarr.map((s) => s.split("").reverse().join(""));
+//  console.log(revsen);
 
-// // // //  ----------------------------------------------------
+//  ----------------------------------------------------
 
-// // //  const arr = ["apple", 11212, "banana", 14, "kiwi", 856, "rose", 4785];
-// // //  const numarr = arr.filter((ele) => typeof ele === "number").reverse();
-// // //  const res = arr.map((ele) => (typeof ele === "number" ? numarr.shift() : ele));
-// // //  console.log(res);
+//  const arr = ["apple", 11212, "banana", 14, "kiwi", 856, "rose", 4785];
+//  const numarr = arr.filter((ele) => typeof ele === "number").reverse();
+//  const res = arr.map((ele) => (typeof ele === "number" ? numarr.shift() : ele));
+//  console.log(res);
 
-// // // //-----------------------------------------------------
+//-----------------------------------------------------
 
-// // // //  1. 
-// // // const inventory = [
-// // //   { id: 1, name: "Tablet", inStock: true },
-// // //   { id: 2, name: "Laptop", inStock: false },
-// // //   { id: 3, name: "Headphones", inStock: false },
-// // //   { id: 4, name: "Charger", inStock: true }
-// // // ];
+//  1. 
+// const inventory = [
+//   { id: 1, name: "Tablet", inStock: true },
+//   { id: 2, name: "Laptop", inStock: false },
+//   { id: 3, name: "Headphones", inStock: false },
+//   { id: 4, name: "Charger", inStock: true }
+// ];
 
-// // // let outofstock = inventory.filter(i => i.inStock === false);
-// // // console.log(outofstock);
+// let outofstock = inventory.filter(i => i.inStock === false);
+// console.log(outofstock);
 
-// // // // 2.
-// // // const doctors = [
-// // //   { id: 1, name: "Dr. Tanya", onlineConsultation: true },
-// // //   { id: 2, name: "Dr. Imran", onlineConsultation: false },
-// // //   { id: 3, name: "Dr. Farah", onlineConsultation: true },
-// // //   { id: 4, name: "Dr. Kunal", onlineConsultation: false }
-// // // ];
+// 2.
+// const doctors = [
+//   { id: 1, name: "Dr. Tanya", onlineConsultation: true },
+//   { id: 2, name: "Dr. Imran", onlineConsultation: false },
+//   { id: 3, name: "Dr. Farah", onlineConsultation: true },
+//   { id: 4, name: "Dr. Kunal", onlineConsultation: false }
+// ];
 
-// // // const onlineDoctors = doctors.filter(doctor => doctor.onlineConsultation);
-// // // console.log(onlineDoctors);
+// const onlineDoctors = doctors.filter(doctor => doctor.onlineConsultation);
+// console.log(onlineDoctors);
 
 // // // // --------------------------------------------------------------------
 // // // // 3. 
