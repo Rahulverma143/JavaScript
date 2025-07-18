a83b93542f9a257d2bde2bd6ca3bc0786e22e168
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-18 13:50:40 +0530

    Question add

---
+++ Test/Que2.js
@@ -0,0 +1,5 @@
+let fruits = ['apple', 'banana', 'cherry'];
+fruits.pop(); // Removes 'cherry' from the end
+console.log(fruits); 
+fruits.shift(); // Removes 'apple' from the beginning
+console.log(fruits);
