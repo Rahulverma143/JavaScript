641be5750cc19c573e2aa7d7a767fd742caa5fdd
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-19 17:02:22 +0530

    Add Que16

---
+++ Test/Que16.js
@@ -0,0 +1,10 @@
+const colors = ["red", "yellow", "blue"];
+colors[5] = "purple";
+colors.forEach((item, index) => {
+  console.log(`${index}: ${item}`);
+});
+// Output:
+// 0: red
+// 1: yellow
+// 2: blue
+// 5: purple
